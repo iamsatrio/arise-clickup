572a4ab76e51eecbe38997684dd5f68776037d7f
Satrio Wibowo <macbook@MACBOOKs-MacBook-Pro.local>
2023-10-30 02:48:57 +0700

    add check in & check out method

--- routes/synchronizer.js
+++ routes/synchronizer.js
@@ -2,45 +2,18 @@ const express = require('express');
 const router = express.Router();
 const synchronizer = require('../services/synchronizer');
 
-router.post('/task_date', async function (req, res, _next) {
+router.post('/checkIn', async function (req, res, _next) {
     try {
-        res.json(await synchronizer.dateSync(req.body.payload, req.query.type));
+        res.json(await synchronizer.checkIn(req.body.payload, req.query.type));
     } catch (err) {
         console.error(err.message);
         res.status(err.statusCode || 500).json({ 'message': err.message });
     }
 });
 
-router.post('/ideation', async function (req, res, _next) {
+router.post('/checkOut', async function (req, res, _next) {
     try {
-        res.json(await synchronizer.relationSync(req.body.payload));
-    } catch (err) {
-        console.error(err.message);
-        res.status(err.statusCode || 500).json({ 'message': err.message });
-    }
-});
-
-router.post('/counter_feedback_staging', async function (req, res, _next) {
-    try {
-        res.json(await synchronizer.counterFeedbackStaging(req.body.payload, req.query.type));
-    } catch (err) {
-        console.error(err.message);
-        res.status(err.statusCode || 500).json({ 'message': err.message });
-    }
-});
-
-router.post('/counter_feedback_prod', async function (req, res, _next) {
-    try {
-        res.json(await synchronizer.counterFeedbackProd(req.body.payload, req.query.type));
-    } catch (err) {
-        console.error(err.message);
-        res.status(err.statusCode || 500).json({ 'message': err.message });
-    }
-});
-
-router.post('/subtask_sync', async function (req, res, _next) {
-    try {
-        res.json(await synchronizer.subtaskSync(req.body.payload, req.query.type));
+        res.json(await synchronizer.checkOut(req.body.payload, req.query.type));
     } catch (err) {
         console.error(err.message);
         res.status(err.statusCode || 500).json({ 'message': err.message });
