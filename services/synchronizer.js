6a8341e5e7d08995649df519962d9417b9f87351
Satrio Wibowo <macbook@MACBOOKs-MacBook-Pro.local>
2023-10-31 03:03:33 +0700

    bugfix duration & comment

--- services/synchronizer.js
+++ services/synchronizer.js
@@ -166,7 +166,7 @@ async function leaveRequest(payload, type) {
             method: "POST",
             url: `https://api.clickup.com/api/v2/task/${task.id}/field/${days_off_cf_id}`,
             data: {
-                "value": days_off+1
+                "value": (days_off/1000)+1
             }
         });
 
